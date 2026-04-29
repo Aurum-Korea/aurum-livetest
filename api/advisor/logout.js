3f8ff98edf7ed424b17e38c699beee7979c13fe7
tkjintl <tkjintl@gmail.com>
2026-04-28 23:17:42 -0400

    wipe repo

--- api/advisor/logout.js
+++
@@ -1,6 +0,0 @@
-import { ok, methodNotAllowed } from '../_lib/http.js';
-export default async function handler(req, res) {
-  if (req.method !== 'POST') return methodNotAllowed(res, ['POST']);
-  res.setHeader('Set-Cookie', 'aurum_advisor=; Path=/; HttpOnly; SameSite=Lax; Max-Age=0');
-  return ok(res, { ok: true });
-}
